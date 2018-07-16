f765e502ccb92215604c08f0a52e8cccc2274ac8
ceeiijMode96 <cubellios96@gmail.com>
2018-07-16 21:43:18 +0800

    initial commit, code structure

---
+++ EmployeeManagement/src/com/project/empmanagement/Test.java
@@ -0,0 +1,10 @@
+package com.project.empmanagement;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
